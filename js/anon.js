00a8dbe31cfbdbdbc1f1bcffc28d814347b4b7aa
Glazelf <gideon.steenbergen@gmail.com>
2020-08-26 03:54:07 +0200

    comment out deprecated code

--- js/anon.js
+++ js/anon.js
@@ -2,28 +2,33 @@ window.onload = function () {
     document.addEventListener("contextmenu", function (e) {
         e.preventDefault();
     }, false);
-    document.addEventListener("keydown", function (e) {
-        //I
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
-            disabledEvent(e);
-        };
-        //J
-        if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
-            disabledEvent(e);
-        };
-        //S/MacOS
-        if (e.keyCode == 83 && (navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey)) {
-            disabledEvent(e);
-        };
-        //U
-        if (e.ctrlKey && e.keyCode == 85) {
-            disabledEvent(e);
-        };
-        //F12
-        if (event.keyCode == 123) {
-            disabledEvent(e);
-        };
-    }, false);
+    // Need to rewrite sometime, due to Javascript deprecating most of the code I used for this it's not functional in its current state.
+    // document.addEventListener("keydown", function (e) {
+    //     // I
+    //     if (e.key == "Control" && e.key == "Shift" && e.key == "I") {
+    //         disabledEvent(e);
+    //     };
+    //     // J
+    //     if (e.key == "Control" && e.key == "Shift" && e.key == "J") {
+    //         disabledEvent(e);
+    //     };
+    //     // S / MacOS
+    //     if (e.key == "S" && e.key == "Meta" && e.key == "Control") {
+    //         disabledEvent(e);
+    //     };
+    //     // CMD OPT I / MacOS
+    //     if (e.key == "Meta" && e.key == "Alt" && e.key == "I") {
+    //         disabledEvent(e);
+    //     };
+    //     // U
+    //     if (e.key == "Control" && e.key == "U") {
+    //         disabledEvent(e);
+    //     };
+    //     // F12
+    //     if (e.key == "F12") {
+    //         disabledEvent(e);
+    //     };
+    // }, false);
     function disabledEvent(e) {
         if (e.stopPropagation) {
             e.stopPropagation();
